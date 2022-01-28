bdab30ee93783030aa2f88cfc4198d4629cbf9b7
Sai Kiran <98076336+Grand-Priest@users.noreply.github.com>
2022-01-28 22:13:58 +0530

    task_4_Sets_MyCaptain
    Using predefined function of set performed different operation like union, intersection, symmetric difference and difference of  two defined sets.

---
+++ Task_4_Sets_AI_MyCaptain.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+E={25,8,97,53,41,5,0,23}
+F={26,74,58,94,8,23,67,88}
+
+
+# In[2]:
+
+
+print("Union of E and F is",E.union(F))
+
+
+# In[4]:
+
+
+print("Intersection of E and F is",E.intersection(F))
+print("Diffence of E and F is",E.difference(F))
+
+
+# In[5]:
+
+
+print("Symmetric difference of E and F",E.symmetric_difference(F))
+
+
+
+
+
+
+
